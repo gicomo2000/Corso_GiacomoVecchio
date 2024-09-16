e93eda4c772d20ee1732c232ea9acd0d3aa39265
gicomo2000 <gicomo2000@icloud.com>
2024-09-16 15:49:18 +0200

    Update es_numpy.py

--- es_numpy.py
+++ es_numpy.py
@@ -15,3 +15,14 @@ print(arr1d[5:15])
 print(arr1d[2::3])
 arr1d[5:10]=99
 print(arr1d)
+
+
+arr2=np.linspace(0,1,12)
+print(arr2)
+re_arr2=arr2.reshape(3,4)
+print(re_arr2)
+casuali=np.random.rand(3,4)
+print(casuali)
+
+somma=re_arr2+casuali
+print(somma)
