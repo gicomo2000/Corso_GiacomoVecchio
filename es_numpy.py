3c9dd4b9cdee57c212b8254edd0f2ac4a5323aae
gicomo2000 <gicomo2000@icloud.com>
2024-09-16 15:11:35 +0200

    Update es_numpy.py

--- es_numpy.py
+++ es_numpy.py
@@ -5,3 +5,13 @@ print(arr.dtype)
 arr=np.array(arr,dtype="float64")
 print(arr.dtype)
 print(arr.shape)
+
+
+arr1d=np.random.randint(10,50,20)
+print(arr1d)
+print(arr1d[:10])
+print(arr1d[-5:])
+print(arr1d[5:15])
+print(arr1d[2::3])
+arr1d[5:10]=99
+print(arr1d)
